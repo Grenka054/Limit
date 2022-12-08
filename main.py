4291eca0b15bea2ea5a5a8222073030e4b101943
Александр <alexgrihalex@gmail.com>
2022-12-09 02:16:37 +0700

    done

--- main.py
+++ main.py
@@ -1,59 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from math import sqrt
-import scipy. stats as stats
+import scipy.stats as stats
+import random
+
+limit = 1000000
 
 
 class ValueError(Exception):
     pass
-
-
-def density(distrib):
-    r = distrib.rv.rvs(size=distrib.limit_val)
-    distrib.r = r
-    _, ax = plt.subplots(1, 1)
-    x = distrib.x_axe()
-    ax.hist(r, density=True, color='blue', edgecolor='black')
-    ax.plot(x, distrib.get_density_plot(x), 'r-', lw=5, label=distrib.name+' density')
-    ax.legend(loc='best', frameon=False)
-    plt.show()
-
-
-def LoLN(distrib):
-    r = distrib.rv.rvs(size=distrib.limit_val)
-    ex = distrib.ex
-    _, ax = plt.subplots(1, 1)
-    exs = []
-    sum = 0
-    for i in range(1, len(r)):
-        sum += r[i]
-        exs.append(sum / i)
-    ax.plot(range(1, len(r)), exs, 'g-', lw=2, label=distrib.name+' E(X)')
-    plt.scatter([len(r) + 1], ex, c='red', marker='*', label='expect', s=40)
-    ax.legend(loc='best', frameon=False)
-    plt.show()
-
-
-def CLT(distrib):
-    limit = 2000000
-    r = distrib.rv.rvs(limit)
-    ex = distrib.ex
-    dx = distrib.dx
-    _, ax = plt.subplots(1, 1)
-    dxs = []
-    sum = 0
-    for i in range(1, len(r)):
-        sum += r[i]
-        dxs.append((sum - i * ex) / sqrt(i * dx))
-        # dxs.append(sum/i)
-    i = 10
-    while i <= limit:
-        print(distrib.name, i, dxs[i])
-        i *= 10
-    ax.plot(range(1, len(r)), dxs, 'g-', lw=2, label=distrib.name+' CLT')
-    # plt.scatter([len(r) + 1], ex, c='red', marker='*', label='expect', s=40)
-    ax.legend(loc='best', frameon=False)
-    plt.show()
 
 
 class Gamma:
@@ -63,16 +18,15 @@ class Gamma:
         self.loc = loc
         self.scale = scale
         self.rv = stats.gamma(k, loc=loc, scale=scale)
-        self.ex = stats.gamma.expect(lambda x: x, (k,), loc=loc, scale=scale)
+        self.ex = stats.gamma.expect(None, (k,), loc=loc, scale=scale)
         self.dx = stats.gamma.var(k, loc=loc, scale=scale)
         self.name = 'gamma'
-        self.limit_val = 2000000
 
     def get_density_plot(self, x):
         return self.rv.pdf(x)
 
     def x_axe(self):
-        return np.linspace(self.rv.ppf(0.0000001), self.rv.ppf(0.9999999), 100)
+        return np.linspace(self.rv.ppf(0.0001), self.rv.ppf(0.9999), 10000)
 
     def check_parameters(self, k, scale):
         if k <= 0:
@@ -87,16 +41,15 @@ class Norm:
         self.loc = loc
         self.scale = scale
         self.rv = stats.norm(loc, scale)
-        self.ex = stats.norm.expect(lambda x: x, loc=loc, scale=scale)
-        self.dx = stats.norm.var(loc=loc, scale=scale)
-        self.name = 'norm'
-        self.limit_val = 4000
+        self.ex = loc
+        self.dx = scale
+        self.name = 'normal'
 
     def get_density_plot(self, x):
         return self.rv.pdf(x)
 
     def x_axe(self):
-        return np.linspace(self.rv.ppf(0.0000001), self.rv.ppf(0.9999999), 100)
+        return np.linspace(self.rv.ppf(0.0001), self.rv.ppf(0.9999), 10000)
 
     def check_parameters(self, scale):
         if scale <= 0:
@@ -111,10 +64,9 @@ class Binom:
         self.loc = loc
         self.scale = scale
         self.rv = stats.binom(n, p)
-        self.ex = stats.binom.expect(lambda x: x, (n, p,), loc, scale)
+        self.ex = stats.binom.expect(None, (n, p), loc, scale)
         self.dx = stats.binom.var(n, p, loc=loc)
         self.name = 'binomial'
-        self.limit_val = 6000
 
     def get_density_plot(self, x):
         return self.rv.pmf(x)
@@ -137,16 +89,15 @@ class Uniform:
         self.loc = a
         self.scale = b - a
         self.rv = stats.uniform(loc=self.loc, scale=self.scale)
-        self.ex = stats.uniform.expect(lambda x: x, loc=self.loc, scale=self.scale)
+        self.ex = stats.uniform.expect(None, loc=self.loc, scale=self.scale)
         self.dx = stats.uniform.var(self.loc, self.scale)
         self.name = 'uniform'
-        self.limit_val = 1000
 
     def get_density_plot(self, x):
         return self.rv.pdf(x)
 
     def x_axe(self):
-        return np.linspace(self.rv.ppf(0.0000001), self.rv.ppf(0.9999999), 100)
+        return np.linspace(self.rv.ppf(0.0001), self.rv.ppf(0.9999), 10000)
 
     def check_parameters(self, a, b):
         if a > b:
@@ -160,11 +111,9 @@ class Poisson:
         self.loc = loc
         self.scale = scale
         self.rv = stats.poisson(mu)
-        self.ex = stats.poisson.expect(lambda x: x, (mu,), loc, scale)
+        self.ex = stats.poisson.expect(None, (mu,), loc, scale)
         self.dx = stats.poisson.var(mu, loc=loc)
         self.name = 'poisson'
-        self.limit_val = 6000
-        self.r = [0]
 
     def get_density_plot(self, x):
         return self.rv.pmf(x)
@@ -186,11 +135,9 @@ class Geom:
         self.loc = loc
         self.scale = scale
         self.rv = stats.geom(p)
-        self.ex = stats.geom.expect(lambda x: x, (p,), loc, scale)
+        self.ex = stats.geom.expect(None, (p,), loc, scale)
         self.dx = stats.geom.var(p, loc=loc)
         self.name = 'geometric'
-        self.limit_val = 6000
-        self.r = []
 
     def get_density_plot(self, x):
         return self.rv.pmf(x)
@@ -214,11 +161,9 @@ class Hypergeom:
         self.loc = loc
         self.scale = scale
         self.rv = stats.hypergeom(M, n, N)
-        self.ex = stats.hypergeom.expect(lambda x: x, (M, n, N,), loc, scale)
+        self.ex = stats.hypergeom.expect(None, (M, n, N,), loc, scale)
         self.dx = stats.hypergeom.var(M, n, N, loc=loc)
-        self.name = 'hgypergeometric'
-        self.limit_val = 6000
-        self.r = []
+        self.name = 'hypergeometric'
 
     def get_density_plot(self, x):
         return self.rv.pmf(x)
@@ -242,48 +187,115 @@ class Cauchy:
         self.loc = loc
         self.scale = scale
         self.rv = stats.cauchy(loc=loc, scale=scale)
-        self.ex = stats.cauchy.expect(lambda x: x, loc=loc, scale=scale)
+        self.ex = stats.cauchy.expect(None, loc=loc, scale=scale)
         self.dx = stats.cauchy.var(loc=loc, scale=scale)
         self.name = 'cauchy'
-        self.limit_val = 6000
 
     def get_density_plot(self, x):
         return self.rv.pdf(x)
 
     def x_axe(self):
-        return np.linspace(self.rv.ppf(0.001), self.rv.ppf(0.999), 100)
+        return np.linspace(self.rv.ppf(0.01), self.rv.ppf(0.99), 100)
+
+
+def calc_density(distrib):
+    x = distrib.x_axe()
+    return x, distrib.get_density_plot(x)
+
+
+def density(distrib):
+    r = distrib.rv.rvs(limit)
+    if distrib.name == 'cauchy':
+        r /= 10000
+    distrib.r = r
+    x, y = calc_density(distrib)
+    _, ax = plt.subplots(1, 1)
+    ax.hist(r, density=True, color='blue', edgecolor='black')
+    ax.plot(x, y, 'r-', lw=5, label=distrib.name + ' density')
+    ax.legend(loc='best', frameon=False)
+    plt.show()
+
+
+def LoLN(distrib):
+    r = distrib.rv.rvs(limit)
+    ex = distrib.ex
+    _, ax = plt.subplots(1, 1)
+    exs = []
+    sum = 0
+    for i in range(limit):
+        sum += r[i]
+        exs.append(sum / (i + 1))
+    ax.plot(range(limit), exs, 'g-', lw=2, label=distrib.name + ' E(X)')
+    plt.scatter(limit, ex, c='red', marker='*', label='expect', s=40)
+    ax.legend(loc='best', frameon=False)
+    plt.show()
+
+
+def CLT(distrib):
+    r = distrib.rv.rvs(limit)
+    ex = distrib.ex
+    dx = distrib.dx
+    _, ax = plt.subplots(1, 1)
+    dxs = []
+    sum = 0
+    for i in range(limit):
+        sum += r[i]
+        dxs.append((sum - (i + 1) * ex) / sqrt((i + 1) * dx))
+    i = 10
+    while i <= limit:
+        print(distrib.name, i, dxs[i - 1])
+        i *= 10
+    ax.plot(range(limit), dxs, 'g-', lw=2, label=distrib.name + ' CLT')
+    ax.legend(loc='best', frameon=False)
+
+
+def check_CLT(distrib, N):
+    n = 100
+    ex = distrib.ex
+    dx = distrib.dx
+    _, ax = plt.subplots(1, 1)
+    y = []
+    for i in range(N):
+        r = distrib.rv.rvs(n)
+        y.append((sum(r) - n * ex) / sqrt(n * dx))
+    ax.hist(y, density=True, color='blue', edgecolor='black', label=distrib.name+' sample')
+    x, y = calc_density(Norm(0, 1))
+    ax.plot(x, y, 'r-', lw=5, label='norm density', alpha=0.5)
+    ax.legend(loc='best', frameon=False)
+    plt.show()
 
 
 if __name__ == '__main__':
     # 1
-    gamma = Gamma(1.99, 0)
-    norm = Norm(1000, 10)
+    gamma = Gamma(1.99, 10)
+    norm = Norm(10, 5)
     binom = Binom(40, 0.2)
     uniform = Uniform(6, 11)
     poisson = Poisson(5)
     geom = Geom(0.7)
     hypergeom = Hypergeom(20, 4, 12)
     cauchy = Cauchy()
-    # density(gamma)
-    # density(norm)
-    # density(binom)
-    # density(uniform)
-    # density(poisson)
-    # density(geom)
-    # density(hypergeom)
-    # density(cauchy)
+
+    density(gamma)
+    density(norm)
+    density(binom)
+    density(uniform)
+    density(poisson)
+    density(geom)
+    density(hypergeom)
+    density(cauchy)
 
     # 2
-    # LoLN(gamma)
-    # LoLN(norm)
-    # LoLN(binom)
-    # LoLN(uniform)
-    # LoLN(poisson)
-    # LoLN(geom)
-    # LoLN(hypergeom)
-    # LoLN(cauchy)  # мат ожидания не существует
+    LoLN(gamma)
+    LoLN(norm)
+    LoLN(binom)
+    LoLN(uniform)
+    LoLN(poisson)
+    LoLN(geom)
+    LoLN(hypergeom)
+    LoLN(cauchy)  # мат ожидания не существует
 
-    # 3
+    # 3 стремится к 0 -> ЗБЧ, больше выборка - больше точность
     CLT(gamma)
     CLT(norm)
     CLT(binom)
@@ -291,3 +303,11 @@ if __name__ == '__main__':
     CLT(poisson)
     CLT(geom)
     CLT(hypergeom)
+
+    check_CLT(gamma, 100000)
+    check_CLT(norm, 100000)
+    check_CLT(binom, 100000)
+    check_CLT(uniform, 100000)
+    check_CLT(poisson, 100000)
+    check_CLT(geom, 100000)
+    check_CLT(hypergeom, 100000)
